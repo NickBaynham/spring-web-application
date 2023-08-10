44bbd2d6cd29198acb11df7d0de3c5266c48bdbf
Nick Baynham <nickbaynham@gmail.com>
2023-08-10 10:03:36 -0400

    [#863hbre6z] Added demo data to the controller.

--- src/main/java/com/example/webapplication/IndexController.java
+++ src/main/java/com/example/webapplication/IndexController.java
@@ -1,12 +1,25 @@
 package com.example.webapplication;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
+
+    record Video(String name) {}
+
+    List<Video> videos = List.of(
+        new Video("Need HELP with your SPRING BOOT 3?"),
+        new Video("Do not do THIS to your own CODE!"),
+        new Video("SECRETS to fix BROKEN CODE!")
+    );
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("videos", videos);
         return "index";
     }
 }
